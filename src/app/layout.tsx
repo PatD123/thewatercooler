d1b400163c196c567c2e0f6e1ece41e410f1538a
Patrick Dai <patrickdai917@gmail.com>
2024-07-28 00:31:41 -0700

    Nav bar is continuously up on all layouts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const poppins = Poppins({ weight: "500", subsets: ["latin"] });
 
@@ -16,7 +17,41 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <div className="navbar">
+          <div className="navbar-start">
+            <a className="btn btn-ghost text-xl">
+              <h1 className="text-xl font-bold text-info">
+                <span className="text-teal-900">the</span>
+                <span className="text-teal-700">watercooler</span>
+              </h1>
+              <Image
+                className="object-fill"
+                src="/favicon.ico"
+                width={35}
+                height={35}
+                alt="Picture of the author"
+              />
+            </a>
+          </div>
+
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <a>Pricing</a>
+              </li>
+              <li>
+                <a>FAQ</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="navbar-end">
+            <a className="btn">Sign In</a>
+          </div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,38 +7,6 @@ export default function Home() {
   const router = useRouter();
   return (
     <div className="bg-gradient-to-r from-amber-500 to-cyan-500">
-      <div className="navbar">
-        <div className="navbar-start">
-          <a className="btn btn-ghost text-xl">
-            <h1 className="text-xl font-bold text-info">
-              <span className="text-teal-900">the</span>
-              <span className="text-teal-700">watercooler</span>
-            </h1>
-            <Image
-              className="object-fill"
-              src="/favicon.ico"
-              width={35}
-              height={35}
-              alt="Picture of the author"
-            />
-          </a>
-        </div>
-
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Pricing</a>
-            </li>
-            <li>
-              <a>FAQ</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="navbar-end">
-          <a className="btn">Sign In</a>
-        </div>
-      </div>
       <div className="hero min-h-screen">
         <div className="hero-content text-center mb-80">
           <div className="max-w-max">
