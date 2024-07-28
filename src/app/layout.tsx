0c31c87b327b1c066ff16d05ba39644ae89d62ea
Patrick Dai <patrickdai917@gmail.com>
2024-07-28 01:14:35 -0700

    Clicking logo brings back to home.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,28 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const poppins = Poppins({ weight: "500", subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "the water cooler",
-  description: "A cool website to see what your friends are currently watching",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
   return (
     <html lang="en">
       <body className={poppins.className}>
         <div className="navbar">
           <div className="navbar-start">
-            <a className="btn btn-ghost text-xl">
+            <a
+              className="btn btn-ghost text-xl"
+              onClick={() => router.push("/")}
+            >
               <h1 className="text-xl font-bold text-info">
                 <span className="text-teal-900">the</span>
                 <span className="text-teal-700">watercooler</span>
