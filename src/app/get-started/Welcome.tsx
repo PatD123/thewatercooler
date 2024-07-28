1040bf18b2282f863575ef27eadadc59a6bf1dd2
Patrick Dai <patrickdai917@gmail.com>
2024-07-28 00:58:22 -0700

    Added temp card placeholder.

--- src/app/get-started/Welcome.tsx
+++ src/app/get-started/Welcome.tsx
@@ -6,7 +6,28 @@ export default function Welcome({ buildProfile }: { buildProfile: any }) {
         <span className="text-teal-900">the</span>
         <span className="text-teal-700">watercooler!</span>
       </div>
-      <div>
+
+      <div className="grid grid-rows-2 grid-cols-1 gap-y-[2.75rem]">
+        <div className="relative">
+          <figure className="absolute">
+            <img className="rounded-lg" src="/pulp.webp" alt="TV/Movie" />
+          </figure>
+          <div className="avatar ml-5 mt-5 absolute">
+            <div className="w-12 rounded-full">
+              <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Jane Doe</h2>
+            <p>Favorite TV Show: New girl</p>
+            <p>Favorite Movie: Pulp Fiction</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="py-8">
         <button className="btn btn-primary" onClick={buildProfile}>
           Build Profile
         </button>
