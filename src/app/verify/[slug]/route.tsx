2ccf46ecf0ab661f75a885a3178e6aa9e0e97d14
Patrick Dai <patrickdai917@gmail.com>
2024-07-31 22:19:54 -0700

    Added proper redirects.

--- src/app/verify/[slug]/route.tsx
+++ src/app/verify/[slug]/route.tsx
@@ -1,20 +1,23 @@
 import User from "@/models/User";
+import { redirect } from "next/navigation";
 
 export async function GET(
   request: Request,
   { params }: { params: { slug: string } }
 ) {
   const slug = params.slug;
-  console.log(slug);
   const user = await User.findOne({ verifStr: slug });
   if (user) {
     user.isValid = true;
     await user.save();
+    redirect("/login");
+  } else {
     const myBlob = new Blob();
-    const myOptions = { status: 200, statusText: "SuperSmashingGreat!" };
+    const myOptions = {
+      status: 401,
+      statusText: "Invalid verification string.",
+    };
     const myResponse = new Response(myBlob, myOptions);
     return myResponse;
-  } else {
-    // Fill in here.
   }
 }
