9ef3936d6f9fd1914ea39a48640c892b90254e21
Patrick Dai <patrickdai917@gmail.com>
2024-08-08 23:31:03 -0700

    Everything on the profile page should now be aligned up?

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -230,7 +230,7 @@ export const Dashboard = () => {
           </div>
 
           <div className="divider divider-horizontal"></div>
-          <div className="w-3/5 max-h-3/4 p-5">
+          <div className="relative w-3/5 max-h-3/4 p-5 flex-col">
             {/* Search Bar */}
             <div className="w-full mx-auto my-8">
               <div className="join w-full">
@@ -244,7 +244,7 @@ export const Dashboard = () => {
                   </div>
                   <ul
                     tabindex="0"
-                    className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+                    className="dropdown-content menu bg-base-100 rounded-box z-50 w-52 p-2 shadow"
                   >
                     <li onClick={() => setCineCategory("Favorite TV Show")}>
                       <a>Favorite TV Show</a>
@@ -278,8 +278,94 @@ export const Dashboard = () => {
                 </div>
               </div>
             </div>
-            <div className="absolute h-full w-full">
-              <h2>Patrick Dai</h2>
+            <div className="absolute flex mt-10 w-full h-full">
+              <div className="flex-none h-96">
+                <div className="sm:col-span-4">
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                      <input
+                        type="text"
+                        name="username"
+                        id="username"
+                        autoComplete="username"
+                        className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                        placeholder="janedoe"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-span-full py-10 h-full">
+                  <label
+                    htmlFor="about"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    About
+                  </label>
+                  <div className="mt-2 h-96">
+                    <textarea
+                      id="about"
+                      name="about"
+                      rows={11}
+                      className="block w-full rounded-md border-5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    ></textarea>
+                  </div>
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Write a few sentences about yourself.
+                  </p>
+                </div>
+              </div>
+
+              <div className="px-10 w-full h-96">
+                <label
+                  htmlFor="cover-photo"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Avatar
+                </label>
+                <div className="mt-2 w-full h-full justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    <svg
+                      className="mx-auto h-12 w-12 text-gray-300"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                    <div className="mt-4 justify-center flex text-sm leading-6 text-gray-600">
+                      <div className="flex">
+                        <label
+                          htmlFor="file-upload"
+                          className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        >
+                          <span>Upload a file</span>
+                          <input
+                            id="file-upload"
+                            name="file-upload"
+                            type="file"
+                            className="sr-only"
+                          />
+                        </label>
+                        <p className="pl-1 text-center">or drag and drop</p>
+                      </div>
+                    </div>
+                    <p className="text-xs leading-5 text-gray-600">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="relative h-full w-full" ref={bentoRef}>
               {bento ? (
