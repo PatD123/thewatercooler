88f9e9bbe340248fd192729305ea9daefa71f895
Patrick Dai <patrickdai917@gmail.com>
2024-09-15 21:40:01 -0400

    Can now select users to recommend movies to.

--- src/app/(dashboard)/dashboard/recommend.tsx
+++ src/app/(dashboard)/dashboard/recommend.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/app/actions/editProfile";
 import Image from "next/image";
 import { useDebouncedCallback } from "use-debounce";
-import SearchUser from "@/app/(dashboard)/friends/searchUser";
+import RecommendUser from "@/app/(dashboard)/dashboard/recommendUser";
 
 // Users to recommend other friends movies and tv shows.
 export default function Recommend({
@@ -47,36 +47,43 @@ export default function Recommend({
 
   return (
     <div
-      className="flex justify-center absolute w-[40%] h-5/6 mt-16 bg-gray-300/30 border-2 border-gray-300 backdrop-blur-2xl rounded-lg shadow-xl z-10"
+      className="flex justify-center absolute w-[30%] min-h-2/5 mt-16 bg-gray-600/30 border-2 border-gray-500/30 backdrop-blur-2xl rounded-lg shadow-xl z-10 px-5"
       ref={searchRef}
     >
-      <div className="flex-grow max-w-md my-3">
-        <div className="flex px-4 py-3 rounded-lg border-2 overflow-hidden mx-auto font-[sans-serif]">
-          <input
-            type="text"
-            placeholder="Search for friends by username..."
-            className="w-full outline-none bg-transparent text-gray-600 text-sm"
-            onChange={updateUserList}
-            onClick={updateUserList}
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 192.904 192.904"
-            width="16px"
-            className="fill-gray-600"
-          >
-            <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
-          </svg>
-        </div>
-        {haveUserList ? (
-          <>
-            <SearchUser
-              queryUser={queryUser}
-              setShowingUser={setShowingUser}
-              setHaveUserList={setHaveUserList}
+      <div className="w-full">
+        <p className="text-cyan-700 text-2xl mt-2">Recommend to a friend.</p>
+
+        {/* Search for User */}
+        <div className="my-2">
+          <div className="flex px-4 py-3 rounded-lg border-2 border-cyan-800 overflow-hidden mx-auto font-[sans-serif]">
+            <input
+              type="text"
+              placeholder="Search for friend ..."
+              className="w-full outline-none bg-transparent text-gray-600 text-sm"
+              onChange={updateUserList}
+              onClick={updateUserList}
             />
-          </>
-        ) : null}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 192.904 192.904"
+              width="16px"
+              className="fill-gray-600"
+            >
+              <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
+            </svg>
+          </div>
+
+          {/* User Search List */}
+          {haveUserList ? (
+            <>
+              <RecommendUser
+                queryUser={queryUser}
+                setShowingUser={setShowingUser}
+                setHaveUserList={setHaveUserList}
+              />
+            </>
+          ) : null}
+        </div>
       </div>
     </div>
   );
---
+++ src/app/(dashboard)/dashboard/recommendUser.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useEffect, useRef } from "react";
+import { getPossibleUsers } from "@/app/actions/getFriends";
+import Image from "next/image";
+
+export default function RecommendUser({
+  queryUser,
+  setShowingUser,
+  setHaveUserList,
+}: {
+  queryUser: string;
+  setShowingUser: any;
+  setHaveUserList: any;
+}) {
+  const [users, setUsers] = useState<any[]>([]);
+  const [selectedUsers, setSelectedUsers] = useState<number[]>([]);
+  const showUsersRef = useRef<any>(null);
+
+  const toggleUserSelection = (id: number) => {
+    setSelectedUsers(
+      (prevSelectedUsers) =>
+        prevSelectedUsers.includes(id)
+          ? prevSelectedUsers.filter((userId) => userId !== id) // Deselect if already selected
+          : [...prevSelectedUsers, id] // Select if not already selected
+    );
+    console.log(selectedUsers);
+  };
+
+  const handleOutsideClick = (e: any) => {
+    if (showUsersRef.current && !showUsersRef.current.contains(e.target)) {
+      setHaveUserList(0);
+    }
+  };
+
+  useEffect(() => {
+    async function getUsers() {
+      const users = await getPossibleUsers(queryUser);
+      if (users) setUsers(users);
+    }
+    getUsers();
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [queryUser]);
+
+  return users ? (
+    <>
+      <div className="h-96 overflow-auto" ref={showUsersRef}>
+        <div className="mt-2">
+          {users.map((user, i) => (
+            <div
+              key={i}
+              className="flex w-full h-12 rounded-lg px-2 mt-1 bg-gray-600/50 hover:bg-gray-400"
+              onClick={() => toggleUserSelection(i)}
+            >
+              <div className="content-center">
+                <div className="relative w-10 h-10">
+                  <Image
+                    className="object-cover rounded-full"
+                    key={i}
+                    src={user["cineImgSrc"]}
+                    alt="Picture of the author"
+                    fill={true}
+                  />
+                </div>
+              </div>
+              <p className="content-center ml-2 text-sm text-slate-800">
+                {user["username"]}
+              </p>
+              {selectedUsers.includes(i) ? (
+                <div className="flex justify-end w-full items-center">
+                  <Image
+                    src="/checkmark.svg"
+                    className="h-8 w-8 "
+                    width={30}
+                    height={30}
+                    alt="checkmark"
+                  />
+                </div>
+              ) : null}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="flex w-full justify-center mt-2 ">
+        <button className="w-full text-lg bg-gray-300/50 text-cyan-800 rounded-lg hover:text-gray-300/50 hover:bg-cyan-600/70">
+          Recommend!
+        </button>
+      </div>
+    </>
+  ) : null;
+}
